7f4951e20c01ac6368102780ad8c2d845f5c9011
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-06-20 22:32:12 -0500

    refactor: format code

--- src/pages/SearchResults/SearchResults.js
+++ src/pages/SearchResults/SearchResults.js
@@ -3,22 +3,22 @@ import { useGifs } from "../../hook/useGifs";
 import ListOfGifs from "../../components/ListOfGifs/ListOfGifs";
 
 export default function SearchResults({ params }) {
-  const { search } = params;
-  const { loading, gifs } = useGifs({ search });
-  console.log("ListOfGifs");
+    const { search } = params;
+    const { loading, gifs } = useGifs({ search });
+    console.log("ListOfGifs");
 
-  return (
-    <>
-      {loading ? 
-      (
-        <div>
-          <p>Loading ...</p>
-        </div>
-      ) : (
+    return (
         <>
-          <ListOfGifs gifs={gifs} />
+            {loading ?
+                (
+                    <div>
+                        <p>Loading ...</p>
+                    </div>
+                ) : (
+                    <>
+                        <ListOfGifs gifs={gifs} />
+                    </>
+                )}
         </>
-      )}
-    </>
-  );
+    );
 }
